c184e5d8405265bcdcb9bb7c221b7d924dc36216
Adam Dipinto <adam.dipinto@gmail.com>
2019-04-09 06:43:50 -0700

    Import Dashboard while setting up a switch and route to connect dashboard component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import AppNavbar from "./components/layout/AppNavBar";
+import Dashbaord from "./components/layout/Dashboard";
 
 import "./App.css";
 
@@ -11,7 +12,9 @@ class App extends Component {
         <div className="App">
           <AppNavbar />
           <div className="container">
-            <h1>Hello</h1>
+            <Switch>
+              <Route exact path="/" component={Dashbaord} />
+            </Switch>
           </div>
         </div>
       </Router>
